970ddce875f1cde271005e4a4f67f5d01edcfdd4
Bruna Caricio <brunacaricio@gmail.com>
2024-08-05 17:04:48 +0200

    find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function(people) {
+  let oldestPerson = people[0];
+  let oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+
+  people.forEach(person => {
+    const currentAge = getAge (person.yearOfBirth, person.yearOfDeath)
+    if (currentAge > oldestAge) {
+      oldestPerson = person;
+      oldestAge = currentAge;
+    }
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
