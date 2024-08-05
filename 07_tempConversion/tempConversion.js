1ce4583638af5209f15172fc70a27266485c9f8d
Bruna Caricio <brunacaricio@gmail.com>
2024-08-05 16:22:33 +0200

    convert f to c and c to f

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemper) {
+  let celsius = (fahrenheitTemper-32) * 5/9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemper) {
+  let fahrenheit = (celsiusTemper * 9/5 + 32);
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
